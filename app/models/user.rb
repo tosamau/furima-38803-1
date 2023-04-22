d9a602844b86630f2fbe8ba67aa78800262851c0
tosamau <voyage33780@gmail.com>
2023-04-22 17:54:00 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
+VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/.freeze
+
  validates :nickname, presence: true
  #validates :password, presence: true, format: { with: /\A(?=.&#042;?[a-z])(?=.&#042;?\d)[a-z\d]+\z/i }
- validates :password, presence: true
+ validates :password, presence: true,  format: { with: VALID_PASSWORD_REGEX }
  validates :family_name, presence: true
  validates :last_name, presence: true
  #validates :family_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
- validates :family_name_kana, presence: true
+ validates :family_name_kana, presence: true, format: { with: KATAKANA_REGEXP }
  #validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
- validates :last_name_kana, presence: true
+ validates :last_name_kana, presence: true, format: { with: KATAKANA_REGEXP }
  validates :birth, presence: true
 
 end
