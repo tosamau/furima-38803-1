89cd9caea5daa145dd48c6a0c163895bb58c2896
tosamau <voyage33780@gmail.com>
2023-04-22 22:07:31 +0900

    バリデーション（エラーメッセージ修正）

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,17 +6,18 @@ class User < ApplicationRecord
 
 
 VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/.freeze
+VALID_KATAKANA_REGEX = /\A[\p{katakana}\p{blank}ー－]+\z/
+VALID_NAME_REGEX =/\A[ぁ-んァ-ン一-龥々]+\z/
 
  validates :nickname, presence: true
  #validates :password, presence: true, format: { with: /\A(?=.&#042;?[a-z])(?=.&#042;?\d)[a-z\d]+\z/i }
- validates :password, presence: true,  format: { with: VALID_PASSWORD_REGEX }
- validates :family_name, presence: true
- validates :last_name, presence: true
+ validates :password, format: { with: VALID_PASSWORD_REGEX}
+ validates :family_name, presence: true, format: {with: VALID_NAME_REGEX, message: "is invalid. Input full-width characters"}
+ validates :last_name, presence: true, format: {with: VALID_NAME_REGEX, message: "is invalid. Input full-width characters"}
  #validates :family_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
- validates :family_name_kana, presence: true, format: { with: KATAKANA_REGEXP }
+ validates :family_name_kana, presence: true, format: { with: VALID_KATAKANA_REGEX, message: "is invalid. Input full-width katakana characters" }
  #validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
- validates :last_name_kana, presence: true, format: { with: KATAKANA_REGEXP }
+ validates :last_name_kana, presence: true, format: { with: VALID_KATAKANA_REGEX, message: "is invalid. Input full-width katakana characters" }
  validates :birth, presence: true
 
 end
