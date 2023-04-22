77d09cb324160595f3bc32681c4993773203f424
tosamau <voyage33780@gmail.com>
2023-04-22 15:43:48 +0900

    ログイン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,6 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 
 
 
-
-
-
   private
 
   def basic_auth
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
  validates :nickname, presence: true
+ #validates :password, presence: true, format: { with: /\A(?=.&#042;?[a-z])(?=.&#042;?\d)[a-z\d]+\z/i }
+ validates :password, presence: true
  validates :family_name, presence: true
  validates :last_name, presence: true
+ #validates :family_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
  validates :family_name_kana, presence: true
+ #validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
  validates :last_name_kana, presence: true
  validates :birth, presence: true
-
- VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
- validates :password, format: { with: VALID_PASSWORD_REGEX }
 
 end
