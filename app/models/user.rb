7ed0fdb0b0f19f069e97de0ea3cbcc007534e8cd
tosamau <voyage33780@gmail.com>
2023-04-26 22:30:43 +0900

    バリデーション修正_03

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,12 @@ class User < ApplicationRecord
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   VALID_KATAKANA_REGEX = /\A[\p{katakana}\p{blank}ー－]+\z/
-  VALID_NAME_REGEX = /\A[一-龯ぁ-んァ-ン]+\z/ 
+  
 
   validates :nickname, presence: true
   validates :password, format: { with: VALID_PASSWORD_REGEX }
-  validates :family_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'is invalid. Input full-width characters' }
-  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'is invalid. Input full-width characters' }
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
   validates :family_name_kana, presence: true,
                                format: { with: VALID_KATAKANA_REGEX, message: 'is invalid. Input full-width katakana characters' }
   validates :last_name_kana, presence: true,
