3aeb0129ee62223efabd03d585c9cfc93b093bb1
tosamau <voyage33780@gmail.com>
2023-04-30 10:20:49 +0900

    商品出品ページ表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,19 +2,27 @@ class ItemsController < ApplicationController
   #before_action :move_to_index, except: [:index, :show]
 
   def index
-    #@items = Item.all
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
   end
 
   private
 
-  private
+  def item_params
+    params.require(:item).permit(:item_name,:description, :category_id, :condition_id, :shipping_fee_id, :ship_from_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
 
-  #def message_params
-    #params.require(:hoge).permit(:hoge, :image).merge(user_id: current_user.id)
- # end
   #def move_to_index
     #return if user_signed_in?
 
     #redirect_to action: :index
   #end
 end
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -18,12 +18,3 @@ class Item < ApplicationRecord
 end
 
 
-t.string       :item_name,        null: false 
-t.text         :description,      null: false 
-t.integer      :category_id,      null: false
-t.integer      :condition_id,     null: false 
-t.integer      :shipping_fee_id,  null: false
-t.integer      :ship_from_id,     null: false
-t.integer      :shipping_day_id,  null: false
-t.integer      :price,            null: false
-t.references   :user,             null:false, foreign_key: true
