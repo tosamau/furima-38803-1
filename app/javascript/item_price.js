550504f0b1e019d5b0a2617373afdf23ae538211
tosamau <voyage33780@gmail.com>
2023-05-06 17:24:42 +0900

    出品レビュー前_02

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,35 +1,15 @@
 window.addEventListener('load', () => {
-  //console.log("Hello");
-
-  
-//const priceInput = document.getElementById("item-price");
-//priceInput.addEventListener("input", () => {
-  //const inputValue = priceInput.value;
-  //console.log(inputValue);
-  //})
-
-//const addTaxDom = document.getElementById("add-tax-price");
-  //addTaxDom.innerHTML = Math.floor(inputvalue);
 
   const priceInput = document.getElementById("item-price");
-priceInput.addEventListener("input", () => {
+  priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
-  const taxValue = Math.floor(inputValue * 0.1); // inputValueの10%を計算する
+  const taxValue = Math.floor(inputValue * 0.1); 
   const addTaxDom = document.getElementById("add-tax-price");
-  addTaxDom.innerHTML = taxValue; // 結果を表示する
+  addTaxDom.innerHTML = taxValue; 
   const profitValue = Math.floor(inputValue - taxValue)
   const addProfitDom = document.getElementById("profit")
   addProfitDom.innerHTML = profitValue;
 })
-
-//const priceInput = document.getElementById("item-price");
-//priceInput.addEventListener("input", () => {
-//  const inputValue = priceInput.value;
-//  const profitValue = Math.floor(inputValue + taxValue);
-//  const addProfitDom  = document.getElementById("profit");
-//  addProfitDom.innerHTML = profitValue; // 結果を表示する
-//})
-
 
 });
 
