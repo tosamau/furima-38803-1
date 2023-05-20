a728067ff434d0c87ff5df5042d38b0f5c6ce1ab
tosamau <voyage33780@gmail.com>
2023-05-20 14:29:04 +0900

    フォームの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,7 @@ class OrdersController < ApplicationController
   end
 
   def create
+    binding.pry
   end
 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,17 +4,17 @@ class OrderAddress
 
   with_options presence: true do
     #validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
-    validates :token, presence: true
-    validates :item_id, presence: true
-    validates :user_id, presence: true
+    validates :token
+    validates :item_id
+    validates :user_id
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :ship_from_id, numericality: { other_than: 1 , message: "can't be blank" }
-    validates :city_name,presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
-    validates :house_number,presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
-    validates :building_name,presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
+    validates :city_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
+    validates :house_number, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
+    validates :building_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
     validates :phone_number,numericality: {only_integer: true, message: "is invalid"}
-    validates :order_id,presence: true
+    validates :order_id
   end
+  validates :ship_from_id, numericality: { other_than: 1 , message: "can't be blank" }
 
     def save
       order = Order.create(item: item_id, user: user_id )
